f37e3247fc5086aacd0f9c76e451781c95136a71
Yurii Pidopryhora <yuretzius@gmail.com>
2026-01-28 18:59:18 +0100

    Update Euler5.py

--- Euler5.py
+++ Euler5.py
@@ -11,22 +11,21 @@ def Erat(N):
     if N == 0 or N == 1 : return []
     elif N == 2: return [2]
     elif N == 3: return [2,3]
-    else:
-        N_bool = np.array([True]*(N+1))
-        N_bool[0] = False
-        N_bool[1] = False
-        #N_bool[2] = True
-        #N_bool[3] = True
-        N_bool[2**2::2] = False # eiminating even numbers
-        p = 3
-        while p*p <= N:
-            # start from p**2
-            # because all the smaller composites have factors <p
-            # and are already eliminated in previous steps
-            N_bool[p**2::p] = False # python doesn't care if ::p goes beyond existing array
-            p = p + 2 # only odd numbers can be primes larger than 3
-            while not N_bool[p]:
-                p = p + 2 # skip p if it has already been eliminated in previous steps
+    N_bool = np.array([True]*(N+1))
+    N_bool[0] = False
+    N_bool[1] = False
+    #N_bool[2] = True
+    #N_bool[3] = True
+    N_bool[2**2::2] = False # eiminating even numbers
+    p = 3
+    while p*p <= N:
+        # start from p**2
+        # because all the smaller composites have factors <p
+        # and are already eliminated in previous steps
+        N_bool[p**2::p] = False # python doesn't care if ::p goes beyond existing array
+        p = p + 2 # only odd numbers can be primes larger than 3
+        while not N_bool[p]:
+            p = p + 2 # skip p if it has already been eliminated in previous steps
     # returns indices of nonzero elements, which in this case
     # ARE the correcponding natural numbers, which were not eliminated
     # Have to use index [0], because for technical reasons it produces a 2D array
